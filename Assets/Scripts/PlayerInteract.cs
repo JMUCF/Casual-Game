0b4944e5004854e0129d7c6eea47ca3e359ffac1
JMUCF <ju398578@ucf.edu>
2024-09-18 01:17:58 -0400

    Added interact button / Android build settings
    -Button turns green when looking at interactable. Pressing it calls level restart atm.
    -Was getting incompatibility error first time launching game on phone. Fixed it with these changes, but it did increase file size by 9MB :(
    https://discussions.unity.com/t/unity-game-not-compatible-with-latest-version-of-android-error/326763/3

---
+++ Assets/Scripts/PlayerInteract.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.UIElements;
+using UnityEngine.UI;
+
+public class PlayerInteract : MonoBehaviour
+{
+    [SerializeField]
+    private GameManager gm;
+
+    private float maxDistance;
+    private bool hitDetected;
+    private bool canInteract;
+
+    private RaycastHit hit;
+    private Vector3 offset;
+    private GameObject button;
+    private Image buttonImage;
+
+    void Start()
+    {
+        offset = new Vector3(0f, -0.25f, 0f);
+        maxDistance = 1.5f;
+        button = GameObject.FindWithTag("InteractButton");
+        buttonImage = button.GetComponent<Image>();
+        buttonImage.color = Color.gray;
+    }
+
+    void FixedUpdate()
+    {
+        hitDetected = Physics.Raycast(transform.position + offset, transform.forward, out hit, maxDistance);
+        if (hitDetected)
+        {
+            Debug.Log("Hit : " + hit.collider.name);
+            if(hit.collider.name == "Sphere")
+            {
+                buttonImage.color = Color.green;
+                canInteract = true;
+            }
+        }
+        else
+        {
+            buttonImage.color = Color.gray;
+            canInteract = false;
+        }
+
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+
+        if (hitDetected)
+            Gizmos.DrawRay(transform.position + offset, transform.forward * hit.distance);
+        else
+            Gizmos.DrawRay(transform.position + offset, transform.forward * maxDistance);
+    }
+
+    public void OnTap()
+    {
+        if(canInteract)
+            gm.LoseState();
+    }
+}
