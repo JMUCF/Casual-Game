176873cc48e89c1edd05a6f830609c0431544ca3
Smug <zikadika2@gmail.com>
2024-09-27 15:37:41 -0400

    Added Features Check Desc
    Added Fade in/Out when dying
    Updated Script Management

--- Assets/Scripts/Game Systems/GameManager.cs
+++ Assets/Scripts/Game Systems/GameManager.cs
@@ -2,18 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using static EnemyHitBox;
 
 public class GameManager : MonoBehaviour
 {
+
     private void OnEnable()
     {
         EnemyHitBox.onPlayerLose += LoseState;
     }
-    private void OnDisable()
-    {
-        EnemyHitBox.onPlayerLose -= LoseState;
-    }
+
     public void LoseState()
+    {
+        Invoke("ChangeScene", 1);
+       
+    }
+    private void ChangeScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
---
+++ Assets/Scripts/Game Systems/UI/FadeInandOut.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeInandOut : MonoBehaviour
+{
+
+    public CanvasGroup fadeBG;  
+    private bool isFading = false;
+    // Start is called before the first frame update
+    private void OnEnable()
+    {
+        EnemyHitBox.onPlayerLose += FadeAway;
+    }
+    private void Start()
+    {
+        fadeBG.alpha = 1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isFading)
+        {
+            FadeOut();
+        }
+        else if(!isFading && fadeBG.alpha > 0)
+        {
+            Fadein();
+        }
+    }
+    void Fadein()
+    {
+        fadeBG.alpha -= 1f * Time.deltaTime;
+    }
+    void FadeOut()
+    {
+        fadeBG.alpha += 1f * Time.deltaTime;
+    }
+    private void FadeAway()
+    {
+        isFading = true;
+    }
+}
