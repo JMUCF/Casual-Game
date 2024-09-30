efd134d60827cd6d6272f7457e1d13c8e99fe834
Chelsea <ch951840@ucf.edu>
2024-09-30 16:00:20 -0400

    Changed build settings and SceneChange.cs

--- Assets/Scripts/SceneChange.cs
+++ Assets/Scripts/SceneChange.cs
@@ -17,18 +17,24 @@ public class SceneChange : MonoBehaviour
 
     public void PlayButton()
     {
-        SetTypes("Suburbs", 10, 4, 45,2);
         StartCoroutine(LoadSceneAsync(1));
     }
+
+    public void SuburbsButton()
+    {
+        SetTypes("Suburbs", 10, 4, 45, 2);
+        StartCoroutine(LoadSceneAsync(2));
+    }
+
     public void PlayCity()
     {
         SetTypes("City", 25, 6, 60,4);
-        StartCoroutine(LoadSceneAsync(1));
+        StartCoroutine(LoadSceneAsync(2));
     }
     public void PlayArmy()
     {
         SetTypes("Army", 50, 8, 90,5);
-        StartCoroutine(LoadSceneAsync(1));
+        StartCoroutine(LoadSceneAsync(2));
     }
     IEnumerator LoadSceneAsync(int sceneID)
     {
